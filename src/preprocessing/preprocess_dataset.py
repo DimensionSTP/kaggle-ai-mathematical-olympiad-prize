efc916c9ddaabdf1f0c05ad3e1419d2c27bc98d5
DimensionSTP <ddang8jh@gmail.com>
2024-06-22 05:41:42 +0900

    feat: parquet file

--- src/preprocessing/preprocess_dataset.py
+++ src/preprocessing/preprocess_dataset.py
@@ -21,7 +21,15 @@ from omegaconf import DictConfig
 def preprocess_dataset(
     config: DictConfig,
 ) -> None:
-    df = pd.read_csv(f"{config.connected_dir}/data/{config.mode}.csv")
+    if config.mode == "train":
+        df = pd.read_parquet(
+            f"{config.connected_dir}/data/camel-ai/math/{config.mode}.parquet"
+        )
+    elif config.mode == "test":
+        df = pd.read_csv(f"{config.connected_dir}/data/{config.mode}.csv")
+    else:
+        raise ValueError(f"Invalid mode: {config.mode}")
+    original_train_df = pd.read_csv(f"{config.connected_dir}/data/train.csv")
     tokenizer = AutoTokenizer.from_pretrained(
         f"{config.custom_data_encoder_path}/{config.pretrained_model_name}"
     )
@@ -29,15 +37,53 @@ def preprocess_dataset(
     def generate_prompt(
         data: str,
     ) -> str:
-        default_system_prompt = "너의 역할은 대화 내용을 요약해주는 요약 전문가야. 다음 사람들의 대화 내용을 보고 적절히 요약해줘."
+        if config.mode == "test":
+            default_system_prompt = f"""
+You are an advanced math problem-solving expert. Your task is to accurately understand the given math problem and provide the solution. The answer to each problem is an integer between 0 and 999. Do not provide explanations, only the final integer answer. Follow these rules:
+
+1. **Problem Understanding**: Understand the problem accurately.
+2. **Provide the Answer**: Directly provide the final integer answer.
+3. **Answer Verification**: Ensure that your answer is correct and falls within the range of 0 to 999.
+4. **No Explanation**: Do not provide step-by-step explanations or any additional information. Only provide the final answer.
+
+### Example Problems:
+
+**example 1**:
+problem:
+{original_train_df[config.data_column_name][3]}
+answer:
+{original_train_df[config.target_column_name][3]}
+
+**example 2**:
+problem:
+{original_train_df[config.data_column_name][4]}
+answer:
+{original_train_df[config.target_column_name][4]}
+
+**example 3**:
+problem:
+{original_train_df[config.data_column_name][5]}
+answer:
+{original_train_df[config.target_column_name][5]}
+"""
+        else:
+            default_system_prompt = """
+You are an advanced math problem-solving expert. Your task is to accurately understand the given math problem and provide a detailed step-by-step explanation and solution. Follow these rules:
+
+1. **Problem Understanding**: Understand the problem accurately, and if necessary, restate the problem for clarity.
+2. **Step-by-Step Explanation**: Explain each step logically, using equations if needed.
+3. **Result Verification**: Verify that the final result is correct and explain the significance of the result.
+4. **Clear Description**: Ensure your explanation is clear and concise, with each step being understandable.
+5. **Provide Additional Information**: Provide additional concepts or information related to the problem if needed.
+"""
         prompt = f"""### Instruction:
-        {default_system_prompt}
+{default_system_prompt} 
 
-        ### Input:
-        {data.strip()}
+### Input(problem):
+{data.strip()}
 
-        ### Response:
-        """.strip()
+### Response(solution):
+""".strip()
         return prompt
 
     df["prompt"] = df[config.data_column_name].apply(generate_prompt)
@@ -68,10 +114,16 @@ def preprocess_dataset(
             f"{config.connected_dir}/data/preprocessed_dataset/{config.pretrained_model_name}",
             exist_ok=True,
         )
-    df.to_csv(
-        f"{config.connected_dir}/data/preprocessed_dataset/{config.pretrained_model_name}/{config.mode}.csv",
-        index=False,
-    )
+    if config.mode == "train":
+        df.to_parquet(
+            f"{config.connected_dir}/data/preprocessed_dataset/{config.pretrained_model_name}/{config.mode}.parquet",
+            index=False,
+        )
+    else:
+        df.to_csv(
+            f"{config.connected_dir}/data/preprocessed_dataset/{config.pretrained_model_name}/{config.mode}.csv",
+            index=False,
+        )
 
 
 if __name__ == "__main__":
