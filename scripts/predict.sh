80e900063b9d44dce58b79577b19d41ea225fac7
DimensionSTP <ddang8jh@gmail.com>
2024-08-05 21:35:08 +0900

    feat: add and adapt left_padding param in scripts

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -5,6 +5,7 @@ is_tuned="untuned"
 strategy="deepspeed_stage_3_offload"
 upload_user="meta-math"
 model_type="MetaMath-7B-V1.0"
+left_padding=False
 quantization_type="origin"
 peft_type="origin"
 data_max_length=508
@@ -21,6 +22,7 @@ do
         strategy=$strategy \
         upload_user=$upload_user \
         model_type=$model_type \
+        left_padding=$left_padding \
         quantization_type=$quantization_type \
         peft_type=$peft_type \
         data_max_length=$data_max_length \
