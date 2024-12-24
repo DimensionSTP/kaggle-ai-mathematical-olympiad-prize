281da025aa6b4874255df9d5651ce9f2f3700aa3
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:37:02 +0900

    add: prepare and upload all scripts

---
+++ scripts/upload_all_to_hf_hub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_preprocessed=False
+is_tuned="untuned"
+strategy="deepspeed_stage_3_offload"
+upload_user="deepseek-ai"
+model_type="deepseek-math-7b-instruct"
+left_padding=False
+quantization_type="origin"
+peft_type="origin"
+data_max_length=508
+target_max_length=4
+precision="bf16"
+batch_size=64
+model_detail="deepseek-math-7b-instruct"
+
+python $path/upload_all_to_hf_hub.py \
+    is_preprocessed=$is_preprocessed \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    left_padding=$left_padding \
+    quantization_type=$quantization_type \
+    peft_type=$peft_type \
+    data_max_length=$data_max_length \
+    target_max_length=$target_max_length \
+    precision=$precision \
+    batch_size=$batch_size \
+    model_detail=$model_detail
